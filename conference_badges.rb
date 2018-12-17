7b6dfead272404e2229d2c546d10bd5600041001
Brian Murillo <brian.j.murillo@gmail.com>
2018-12-17 19:35:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  badges =[]
+  arr.each do |i|
+    badges << badge_maker(i)
+  end
+  return badges
+end
+
+def assign_rooms(arr)
+  
+  arr.each_with_index do |i|
+    "Hello, #{}"
+  end
+end
